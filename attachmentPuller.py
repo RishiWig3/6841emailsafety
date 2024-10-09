f6cd3d7f831aa102bbe253eaaefa672db50416f5
RishiWig3 <rishi.wig@gmail.com>
2024-10-09 11:15:54 +1100

    latest updates in email pull

--- attachmentPuller.py
+++ attachmentPuller.py
@@ -1,30 +1,23 @@
+from imapclient import IMAPClient
+import email
 import os
-from imbox import Imbox # pip install imbox
-import traceback
 
-host = "imap.proton.com"
-username = "mevvieReceiver@proton.me"
-password = 'yH,,sN^SF2c@8K2'
-port = 993
-# download_folder = "/path/to/download/folder"
+try:
+    server = IMAPClient('localhost', port=1143, ssl=False)  # Port configured by ProtonMail Bridge
+    server.login('mevvieReceiver@proton.me', 'YziUP5FKGf1sryjvTOaDsQ')
+    print("Connected to the server")
 
-# if not os.path.isdir(download_folder):
-#     os.makedirs(download_folder, exist_ok=True)
-    
-mail = Imbox(host, username=username, password=password, ssl=True, ssl_context=None, starttls=False, port=port)
-messages = mail.messages() # defaults to inbox
+    server.select_folder('INBOX')
+    messages = server.search(['UNSEEN'])
 
-for (uid, message) in messages:
-    mail.mark_seen(uid) # optional, mark message as read
+    for uid, message_data in server.fetch(messages, 'RFC822').items():
+        email_message = email.message_from_bytes(message_data[b'RFC822'])
+        for part in email_message.walk():
+            if part.get_content_disposition() == 'attachment':
+                filename = part.get_filename().strip()  # Clean the filename
+                save_path = os.path.join(os.path.expanduser("~"), "Documents/Misc", filename)  # Correct path
+                with open(save_path, 'wb') as f:
+                    f.write(part.get_payload(decode=True))
 
-    # for idx, attachment in enumerate(message.attachments):
-    #     try:
-    #         att_fn = attachment.get('filename')
-    #         download_path = f"{download_folder}/{att_fn}"
-    #         print(download_path)
-    #         with open(download_path, "wb") as fp:
-    #             fp.write(attachment.get('content').read())
-    #     except:
-    #         print(traceback.print_exc())
-
-mail.logout()
+except Exception as e:
+    print(f"An error occurred: {e}")
