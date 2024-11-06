778dbe483186ae658c26525d93f24bbcad208c6c
RishiWig3 <rishi.wig@gmail.com>
2024-11-06 18:50:28 +1100

    some extra resources on the email downloader

--- src/emailRetrieval.py
+++ src/emailRetrieval.py
@@ -19,3 +19,41 @@ def notYet():
                 with open(save_path, 'wb') as f:
                     f.write(part.get_payload(decode=True))
 
+
+
+
+
+# import email
+# import imaplib
+# import os
+
+# class FetchEmail():
+
+#     connection = None
+#     error = None
+
+#     def __init__(self, mail_server, username, password):
+#         self.connection = imaplib.IMAP4_SSL(mail_server)
+#         self.connection.login(username, password)
+#         self.connection.select(readonly=False) # so we can mark mails as read
+
+#     def close_connection(self):
+#         """
+#         Close the connection to the IMAP server
+#         """
+#         self.connection.close()
+
+#     def save_attachment(self, msg, download_folder="/tmp"):
+#         """
+#         Given a message, save its attachments to the specified
+#         download folder (default is /tmp)
+
+#         return: file path to attachment
+#         """
+#         att_path = "No attachment found."
+#         for part in msg.walk():
+#             if part.get_content_maintype() == 'multipart':
+#                 continue
+#             if part.get('Content-Disposition') is None:
+#                 continue
+# https://stackoverflow.com/questions/6225763/downloading-multiple-attachments-using-imaplib
